ca7ac748d317592f966fe5322c0bcbf31e667324
KimTaehyeong <rlaxogud0909@likelion.org>
2021-07-09 09:34:02 +0900

    7/9 3ban class commit 1

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# 장고에서 제공해주는 모델
+class NewModel(models.Model):
+    # 문자열 필드 (null=False는 무조건 값을 넣어줘야한다는 뜻)
+    text = models.CharField(max_length=255, null=False)
