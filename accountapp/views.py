1b641f43b267dca11b34944093e9e4cb9b3f94f1
KimTaehyeong <rlaxogud0909@likelion.org>
2021-07-09 10:17:09 +0900

    7/9 3ban class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# request에 요청관련 메서드가 다 들어가 있다
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+        # context
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text':'POST METHOD!'})
+    # Get 방식으로 올때도 정의를 해줘야 에러가 안뜸
+    else:
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text':'GET METHOD!'})
