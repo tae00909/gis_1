1f3dea1def0ac5fad6b01ba3c63f6d84c1494de6
KimTaehyeong <rlaxogud0909@likelion.org>
2021-07-09 10:20:25 +0900

    7/9 3ban class commit 2.75

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 # request에 요청관련 메서드가 다 들어가 있다
 def hello_world(request):
     if request.method == "POST":
-        # context
+        # context = 딕셔너리, key 값이 템플릿에서 사용할 변수 이름, value 값이 파이썬 변수가 됨
         return render(request, 'accountapp/hello_world.html',
                       context={'text':'POST METHOD!'})
     # Get 방식으로 올때도 정의를 해줘야 에러가 안뜸
