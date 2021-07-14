a0e861c5c722aa9fd5730a693fe96ec532fcd842
KimTaehyeong <rlaxogud0909@likelion.org>
2021-07-14 15:58:02 +0900

    7/14 3ban class commit 3

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
@@ -7,5 +8,12 @@ app_name = 'accountapp'
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
 
+    # 장고에서 제공해주는 LoginView
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'),
+         name='login'),
+    # 로그아웃
+    path('logout/',LogoutView.as_view(), name='logout'),
+
+    # AccountCreateView는 클래스이기 때문에 as_view를 추가
     path('create/', AccountCreateView.as_view(), name='create')
 ]
