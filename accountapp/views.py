80067704fb351c24fa4492b914a9d3a9c7d215dd
KimTaehyeong <rlaxogud0909@likelion.org>
2021-07-14 14:49:43 +0900

    7/14 3ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 
 # request에 요청관련 메서드가 다 들어가 있다
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import NewModel
 
@@ -31,3 +34,10 @@ def hello_world(request):
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
                       context={'data_list': data_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp/hello_world')
+    template_name = 'accountapp/create.html'
+
