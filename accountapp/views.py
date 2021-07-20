1515eec998b7c6828783692697158a7c436808d9
KimTaehyeong <rlaxogud0909@likelion.org>
2021-07-20 09:47:54 +0900

    7/20 3ban class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 
 # request에 요청관련 메서드가 다 들어가 있다
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from accountapp.models import NewModel
 
@@ -45,4 +45,11 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = UserCreationForm
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
 
