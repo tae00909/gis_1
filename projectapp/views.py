51cc768f52a18830adab9bfc78538a880793fbdf
KimTaehyeong <rlaxogud0909@likelion.org>
2021-08-17 10:12:35 +0900

    8/17 3ban class commit 1

--- projectapp/views.py
+++ projectapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView
+
+from projectapp.forms import ProjectCreationForm
+from projectapp.models import Project
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    form_class = ProjectCreationForm
+    success_url = reverse_lazy('articleapp:list') #임시
+    template_name = 'projectapp/create.html'
+
+class ProjectDetailView(DetailView):
+    model = Project
+    context_object_name = 'target_project'
+    template_name = 'projectapp/detail.html'
