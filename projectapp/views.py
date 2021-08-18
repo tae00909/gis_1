a531bcd6121144aa875f8ee13955ab9aa10185d9
rlaxogud0909 <rlaxogud0909@likelion.org>
2021-08-18 15:44:42 +0900

    8/18 3ban class commit 3

--- projectapp/views.py
+++ projectapp/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic.list import MultipleObjectMixin
 
+from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
@@ -20,10 +22,18 @@ class ProjectCreateView(CreateView):
     def get_success_url(self):
         return reverse('projectapp:detail', kwargs={'pk':self.object.pk})
 
-class ProjectDetailView(DetailView):
+class ProjectDetailView(DetailView, MultipleObjectMixin):
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
+    #MultipleObjectMixin를 적으면 paginate_by 사용가능
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        # Article 모델에서 만든 project가 현재 오브젝트(target_project)와 같은 것만 필터
+        article_list = Article.objects.filter(project=self.object)
+        # object_list에 위에서 뽑은 article_list 할당
+        return super().get_context_data(object_list = article_list, **kwargs)
 
 class ProjectListView(ListView):
     model = Project
