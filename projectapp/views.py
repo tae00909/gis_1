750f80afa2b3ba42893e056a4e2947b46361c4f3
rlaxogud0909 <rlaxogud0909@likelion.org>
2021-08-20 15:40:01 +0900

    8/20 3ban class commit 3

--- projectapp/views.py
+++ projectapp/views.py
@@ -10,6 +10,7 @@ from django.views.generic.list import MultipleObjectMixin
 from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
+from subscribeapp.models import Subscription
 
 
 @method_decorator(login_required, 'get')
@@ -30,10 +31,22 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
     paginate_by = 20
 
     def get_context_data(self, **kwargs):
+        # 구독이 됐는지 안됐는지
+        user = self.request.user
+        project = self.object
+
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user,
+                                                       project=project)
+        else:
+            subscription = None
+
         # Article 모델에서 만든 project가 현재 오브젝트(target_project)와 같은 것만 필터
         article_list = Article.objects.filter(project=self.object)
         # object_list에 위에서 뽑은 article_list 할당
-        return super().get_context_data(object_list = article_list, **kwargs)
+        return super().get_context_data(object_list = article_list,
+                                        subscription=subscription,
+                                        **kwargs)
 
 class ProjectListView(ListView):
     model = Project
