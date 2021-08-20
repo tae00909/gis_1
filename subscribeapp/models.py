5c6646d83fd80b723da4268f6986d7c704f19cd0
rlaxogud0909 <rlaxogud0909@likelion.org>
2021-08-20 14:47:30 +0900

    8/20 3ban class commit 1

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='subscription', null=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,
+                                related_name='subscription', null=False)
+
+    class Meta:
+        # 지정한 두 쌍이 유일하게 하나만 설정하게 함
+        unique_together = ['user', 'project']
